0d7dd25d0a070000e61afa2afd7381a04cbc6433
Julian Vennen <julian@aternos.org>
2024-06-18 18:55:16 +0200

    Add client side command /mclogsc

---
+++ src/main/java/gs/mclo/fabric/commands/source/ClientSource.java
@@ -0,0 +1,34 @@
+package gs.mclo.fabric.commands.source;
+
+import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
+import net.minecraft.text.Text;
+
+import java.nio.file.Path;
+
+public class ClientSource implements Source {
+    private final FabricClientCommandSource parent;
+
+    public ClientSource(FabricClientCommandSource parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public String getMinecraftVersion() {
+        return parent.getClient().getGameVersion();
+    }
+
+    @Override
+    public void sendFeedback(Text message, boolean broadcastToOps) {
+        parent.sendFeedback(message);
+    }
+
+    @Override
+    public void sendError(Text message) {
+        parent.sendError(message);
+    }
+
+    @Override
+    public Path getRunDirectory() {
+        return parent.getClient().runDirectory.toPath();
+    }
+}
---
+++ src/main/java/gs/mclo/fabric/commands/source/ServerSource.java
@@ -0,0 +1,34 @@
+package gs.mclo.fabric.commands.source;
+
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+
+import java.nio.file.Path;
+
+public class ServerSource implements Source {
+    private final ServerCommandSource parent;
+
+    public ServerSource(ServerCommandSource parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public String getMinecraftVersion() {
+        return parent.getMinecraftServer().getVersion();
+    }
+
+    @Override
+    public void sendFeedback(Text message, boolean broadcastToOps) {
+        parent.sendFeedback(message, broadcastToOps);
+    }
+
+    @Override
+    public void sendError(Text message) {
+        parent.sendError(message);
+    }
+
+    @Override
+    public Path getRunDirectory() {
+        return parent.getMinecraftServer().getRunDirectory().toPath();
+    }
+}
