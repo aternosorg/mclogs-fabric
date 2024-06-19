beb68b79ef86861fdaff7d934233f22b41827f10
Julian Vennen <julian@aternos.org>
2024-06-19 19:31:57 +0200

    Update to 1.20.6

--- src/main/java/gs/mclo/fabric/MclogsFabric.java
+++ src/main/java/gs/mclo/fabric/MclogsFabric.java
@@ -25,7 +25,6 @@ import net.minecraft.util.Formatting;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.impl.MutableLogEvent;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
